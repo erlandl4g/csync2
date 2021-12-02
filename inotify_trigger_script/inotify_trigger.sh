78d3ddf80eb32f28ea67ec74bf4ee52ef4afd0af
frithrah <simon@frithrah.com>
2021-12-02 13:07:25 +0000

    Automatic queue file reset
    Added a setting to reset the queue log file after X number of lines.
    This only triggers when the queue is quiet. It safely truncates the
    file in place and then performs a full sync in case inotify added to
    the queue between the check and the reset. A full reset every now and
    then seems like a good idea anyway.
    
    It's not possible to run a sed command to delete only the lines read so
    far because --in-place actually swaps out the file which messes up the
    inotify writing process (understandably). I tested this extensively in
    various ways and found queued entries were lost if the queue file was
    altered outside of the process. Locks didn't help either (perhaps
    because of the way IO is buffered) and they slowed things down
    dramatically.

--- inotify_trigger_script/inotify_trigger.sh
+++ inotify_trigger_script/inotify_trigger.sh
@@ -8,12 +8,14 @@ check_interval=0.5                               # Time between queue check in s
 file_events="close_write,move,delete"            # File events to monitor - no spaces in this list
 queue_file=/home/learn4gd/tmp/inotify_queue.log  # File used for event queue
 
+num_lines_until_reset=200000  # Reset queue log file after reading this many lines
+
 cfg_path=/usr/local/etc
 cfg_file=csync2.cfg
 
 csync_opts="$*"
 
-# Start csync server - use subshell so it terminates when script is killed
+# Start csync server - use subshell so it terminates when script exits
 # TODO: Separate hostname from other csync options so it can be used here exclusively
 (csync2 -ii $csync_opts) &
 
@@ -64,46 +66,65 @@ truncate -s 0 $queue_file
 	done
 ) &
 
-# Run a full check and sync before queue monitoring begins
-echo "* INITIAL CSYNC CHECK"
-csync2 $csync_opts -x
-echo " * Done"
+# Run a full check and sync operation
+function csync_full_check()
+{
+	echo "* FULL CSYNC CHECK"
+	csync2 $csync_opts -x
+	echo "  - Done"
+}
 
-queue_line_pos=1
+# Run a full check and sync before queue monitoring begins
+csync_full_check
 
 # Periodically monitor inotify queue file
+queue_line_pos=1
 while true
 do
 	# Delay between updates to allow for batches of inotify events to be gathered
 	sleep $check_interval
 
-	# Make array starting from last position in queue file
-	mapfile -t file_queue < <(tail --lines=+$queue_line_pos $queue_file)
+	# Make array starting from last read position in queue file
+	mapfile -t file_list < <(tail --lines=+$queue_line_pos $queue_file)
 
-	if [[ ${#file_queue[@]} -eq 0 ]]
+	if [[ ${#file_list[@]} -eq 0 ]]
 	then
-		# No new entries - back to waiting
+		# No new entries - time to check for reset
+		if [[ $queue_line_pos -ge $num_lines_until_reset ]]
+		then
+			# Reset queue file
+			echo "* RESET QUEUE"
+			truncate -s 0 $queue_file
+			queue_line_pos=1
+
+			# Run a full sync in case inotify added after read
+			csync_full_check
+		fi
+		# Jump back to sleep
 		continue
 	fi
 
-	echo "* READING QUEUE ($queue_line_pos)"
+	echo "* PROCESSING QUEUE ($queue_line_pos)"
 
 	# Advance queue file position
-	((queue_line_pos+=${#file_queue[@]}))
+	((queue_line_pos+=${#file_list[@]}))
 
 	# Remove duplicates
-	mapfile -t csync_files < <(printf "%s\n" "${file_queue[@]}" | sort -u)
+	mapfile -t csync_files < <(printf "%s\n" "${file_list[@]}" | sort -u)
+
+	# DEBUG: Output files processed in each cycle
+	# printf "%s\n" "${csync_files[@]}" >> "/tmp/csync_$(date +%s%3N).log"
 
 	# Process queue with csync
 	# Split into two stages so that outstanding dirty files can be processed regardless of when or where they were marked
 
 	#   1. Check and possibly mark queued files as dirty
-	echo " * Checking ${#csync_files[@]} files"
+	echo "  - Checking ${#csync_files[@]} files"
 	csync2 $csync_opts -c "${csync_files[@]}"
 
 	#   2. Update outstanding dirty files on peers
-	echo " * Updating all dirty files"
+	echo "  - Updating all dirty files"
 	csync2 $csync_opts -u
 
-	echo " * Done"
+	echo "  - Done"
 done
